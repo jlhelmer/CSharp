ac64df1fb8b5d0ae56e3c621228b07053cd0c6b8
jlhelmer <jessica.helmer@outlook.com>
2017-08-01 11:12:37 -0700

    Fish Tank PH
    Console program to determine whether fish tank is neutral, acidic, or alkaline.

---
+++ FishTankPh.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch5Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Allows user to input PH level of tank
+            Console.WriteLine("Enter fish tank PH level (0 - 14): ");
+            double tankPH;
+
+            //Converts user input into integer 
+            string ph = Console.ReadLine();
+            tankPH = double.Parse(ph);
+
+            //if statement decides if water is neutral, alkaline, 
+            //or acidic. Starts with most limiting factor.
+            //Prints result to console.
+            if (tankPH == 7)
+            {
+                Console.WriteLine("Tank is neutral.");
+            }
+
+            else if (tankPH > 7)
+            {
+                Console.WriteLine("Tank is alkaline.");
+            }
+
+            else
+            {
+                Console.WriteLine("Tank is acidic.");
+            }
+            
+            Console.ReadKey();
+
+        }
+    }
+}
